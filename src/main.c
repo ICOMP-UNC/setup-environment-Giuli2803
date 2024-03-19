15033ad6168472f5f7705c675226c8a6cf9011e8
Giuli2803 <giulianopalombarini@gmail.com>
2024-03-19 15:53:08 -0300

    -Add and link libraries.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h> //Libreria estandar (busca en el system path)
 //#include "../include/greetings.h" // Libreria creada (referenciada por PATH)
 #include <greetings.h>
+#include "shared.h"
+#include "static.h"
 
 int main(int argc, char* argv[])
 {
     printf("Hello, OS II.\n");
     greetings();
+    printf("Calling shared lib from main program. I get '%ld'\n", shared_lib_function("Hello, OS II"));
+    printf("Calling static lib from main program. I get '%ld'\n", static_lib_function("Bye, OS II"));
     return 0;
 }
